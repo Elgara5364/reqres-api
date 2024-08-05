8eec2945f6648c0a38889ce660b8f6043eaf093c
Elgara5364 <steveloardi5364@gmail.com>
2024-08-06 00:47:15 +0900

    initial commit

---
+++ src/app/users/page.jsx
@@ -0,0 +1,83 @@
+"use client";
+
+import axios from "axios";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const User = ({ params }) => {
+  console.log(params);
+  const [page, setPage] = useState(1);
+  const [data, setData] = useState([
+    // {
+    //   id: null,
+    //   name: "",
+    //   src: "",
+    // },
+  ]);
+  const handleGetUser = async () => {
+    try {
+      const res = await axios.get(`https://reqres.in/api/users?page=${page}`);
+      // console.log(res.data.data);
+      setData(res.data.data);
+      // console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+
+    // axios
+    //   .get(`https://reqres.in/api/users?page=1`)
+    //   .then((res) => {
+    //     console.log(res.data.data);
+    //     setData(res.data.data);
+    //     // console.log(data);
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //   });
+  };
+
+  useEffect(() => {
+    handleGetUser();
+    // console.log(data);
+  }, [page]);
+  console.log(data);
+
+  const handleNext = () => {
+    setPage(page + 1);
+  };
+
+  const handleprev = () => {
+    setPage(page - 1);
+  };
+
+  return (
+    <>
+      <h1>User Page</h1>
+      <div className="flex gap-5">
+        <button onClick={handleprev}>prev page</button>
+        <button onClick={handleNext}>next page</button>
+      </div>
+      {data.map((user, idx) => (
+        <div key={idx} className="mb-5">
+          <h1>{user.first_name}</h1>
+          <Image
+            src={user.avatar}
+            alt={user.first_name}
+            sizes="100"
+            width={400}
+            height={400}
+            style={{ sizes: "80" }}
+          />
+        </div>
+      ))}
+    </>
+  );
+};
+
+// export const GET = async () => {
+//   const users = Response.json("http://reqres.in/api/users?page=1");
+//   console.log(users);
+//   return users;
+// };
+
+export default User;
